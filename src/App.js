9954f116f97ec2fc0f2df2c330d5d2db6496c4e4
Dhinesh <a.dhinesh1@gmail.com>
2024-01-29 00:10:36 +0530

    adjust code alignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <span>
-        <input type='text'></input>
-        <button>Add to do</button>
+          <input type="text"></input>
+          <button>Add to do</button>
         </span>
       </header>
     </div>
